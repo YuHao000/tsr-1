695936aef343215cf8a1d96b0f1f92b0f5302b2d
liyi <liyi_whu@163.com>
2018-11-05 01:01:38 +0800

    add detect function

--- src/CPointSet.cpp
+++ src/CPointSet.cpp
@@ -11,29 +11,53 @@ namespace tsr
         m_ly = h;
         m_rx = 0;
         m_ry = 0;
-        //m_bs.SetSize(w * h);
+        m_greenNum = 0;
+        m_redNum = 0;
+        m_type = PS_UNMARK;
     }
 
     CPointSet::~CPointSet()
     {
     }
 
-    void CPointSet::Add(int x, int y)
+    void CPointSet::Add(int x, int y, CColor& color)
     {
         m_ps.push_back(std::make_pair(x, y));
         m_lx = MIN(m_lx, x);
         m_ly = MIN(m_ly, y);
         m_rx = MAX(m_rx, x);
         m_ry = MAX(m_ry, y);
+
+        if (color.IsGreen()) {
+            m_greenNum++;
+        }
+        else if (color.IsRed()) {
+            m_redNum++;
+        }
     }
 
-    void CPointSet::push_back(int x, int y)
+    void CPointSet::push_back(int x, int y, CColor& color)
     {
-        Add(x, y);
+        Add(x, y, color);
     }
 
     std::pair<int, int>& CPointSet::operator[](int i)
     {
         return m_ps[i];
     }
+
+    //PS_TYPE_E CPointSet::GetType()
+    //{
+    //    /* 如果绿点占了90%以上，则认为是绿灯 */
+    //    if ((float)m_greenNum > (m_ps.size() * 0.9)) {
+    //        return PS_GREEN_LIGHT;
+    //    }
+
+    //    /* 红绿灯是实心 */
+    //    if (m_ps.size() > m_rect.Aera() * 0.5) {
+    //        return PS_RED_LIGHT;
+    //    }
+
+
+    //}
 }
--- src/CPointSet.hpp
+++ src/CPointSet.hpp
@@ -1,22 +1,40 @@
 #pragma once
 
 #include <vector>
+#include <bitset>
 #include "CRect.hpp"
 #include "CBitset.hpp"
+#include "CColor.hpp"
 
 namespace tsr
 {
+    enum PS_TYPE_E{
+        PS_RED_LIGHT = 0,
+        PS_GREEN_LIGHT,
+        PS_SPEED_LIMIT,
+        PS_FORBID_TURN,
+        PS_UNMARK,  /*Î´Ê¶±ð¹ý*/
+        PS_OTHER
+    };
     class CPointSet
     {
     public:
         CPointSet(int w, int h);
         ~CPointSet();
 
-        void Add(int x, int y);
+        void Add(int x, int y, CColor& color);
         void CalcRect() { m_rect.Set(m_lx, m_ly, m_rx - m_lx, m_ry - m_ly); }
         CRect &GetRect() { return m_rect; }
 
-        void push_back(int x, int y);
+        int GetRNum() { return m_redNum; }
+        int GetGNum() { return m_greenNum; }
+
+        int GetRectArea() { return m_rect.Aera(); }
+
+        void  SetType(PS_TYPE_E type) { m_type = type; }
+        PS_TYPE_E GetType() { return m_type; }
+
+        void push_back(int x, int y, CColor& color);
         int  size() { return m_ps.size(); }
 
         std::pair<int, int>& operator[](int i);
@@ -28,7 +46,10 @@ namespace tsr
         int                                 m_ly;
         int                                 m_rx;
         int                                 m_ry;
-        CBitset                             m_bs;   // bit set
+        int                                 m_redNum;
+        int                                 m_greenNum;
+
+        PS_TYPE_E                           m_type;
     };
 }
 
